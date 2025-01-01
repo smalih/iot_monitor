52e918c76fbac28d2f559a611d473879d36f20eb
Solomon Malih <smalih15@hotmail.com>
2025-01-01 12:07:59 +0000

    updated database code to include device type; added endpoint to receice device info in JSON format and add to database

--- rbp_server/server.py
+++ rbp_server/server.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 
 
 load_dotenv()
@@ -33,6 +33,7 @@ cur.execute("DROP TABLE IF EXISTS devices, packets;")
 cur.execute("CREATE TABLE devices (mac_addr macaddr PRIMARY KEY,"
                                     "ip_addr inet UNIQUE NOT NULL,"
                                     "name varchar (60) NOT NULL,"
+                                    "type varchar (60) NOT NULL,"
                                     "status varchar (8) DEFAULT 'SECURE');"
                                     )
 
@@ -59,12 +60,33 @@ conn.close()
 def get_devices():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM devices;")
+    cur.execute("SELECT mac_addr, ip_addr FROM devices;")
     devices = cur.fetchall()
     cur.close()
     conn.close()
     print(devices)
     return devices
+
+@app.route("/add", methods=['POST'])
+def add_device():
+    device_info = request.json
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO devices (mac_addr, ip_addr, name, type, status)"
+                "VALUES (%s, %s, %s, %s)",
+                (device_info['macAddr'],
+                device_info['ipAddr'],
+                device_info['name'],
+                device_info['type'],
+                device_info['status'])
+                )
+    
+    cur.close()
+    conn.close()
+    return redirect(url_for("index"))
+
+
+
 
 @app.route("/")
 def index():
