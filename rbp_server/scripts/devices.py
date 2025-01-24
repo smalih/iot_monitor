d191e7c2a7675c3031814e10f05d08d3fe6ab6a6
Solomon Malih <smalih15@hotmail.com>
2025-01-24 13:13:02 +0000

    updated devices python file - made into function and reading file by opening

--- rbp_server/scripts/devices.py
+++ rbp_server/scripts/devices.py
@@ -1,4 +1,27 @@
+import os
+import sys
 import subprocess
-print("getting devices")
-subprocess.call("~/iot_monitor/rbp_server/scripts/devices.sh", shell=True)
-print("done")
+import time
+
+leases_path = "/var/lib/NetworkManager/dnsmasq-wlan0.leases"
+
+def update_device_list(server_path):
+    print(server_path)
+    script_path = os.path.join(server_path, "scripts/devices.sh")
+    try:
+        with open(leases_path, 'r') as leases_file:
+            devices = leases_file.readlines()
+    except FileNotFoundError:
+        print("File not found", file=sys.stderr)
+    except IOError:
+        print("An error while reading leases file. Please try again")
+
+    print(script_path)
+    print("getting devices...")
+    start = time.time()
+    subprocess.call(script_path, shell=True)
+    stop = time.time()
+    print(f"DONE ({(stop - start)*1000}ms)")
+
+
+update_device_list("~/iot_monitor/rbp_server")
