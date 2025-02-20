a0f85e4ea19c4c856e857c4c3d8f6055a48361ef
Solomon Malih <smalih15@hotmail.com>
2025-02-20 14:42:45 +0000

    flask -> fastapi migration basic get request working

--- rbp_server/scripts/devices.py
+++ rbp_server/scripts/devices.py
@@ -8,15 +8,17 @@ leases_path = "/var/lib/NetworkManager/dnsmasq-wlan0.leases"
 def update_device_list(server_path):
     print(server_path)
     script_path = os.path.join(server_path, "scripts/devices.sh")
-    try:
-        with open(leases_path, 'r') as leases_file:
-            devices = leases_file.readlines()
-    except FileNotFoundError:
-        print("File not found", file=sys.stderr)
-    except IOError:
-        print("An error while reading leases file. Please try again")
-
     print(script_path)
+    # try:
+    #     with open(leases_path, 'r') as leases_file:
+    #         devices = leases_file.readlines()
+    #         print("lines read")
+    # except FileNotFoundError:
+    #     print("File not found", file=sys.stderr)
+    # except IOError as e:
+    #     print(f"An error occured while reading leases file. Please try again ({e})")
+
+    # print(script_path)
     print("getting devices...")
     start = time.time()
     subprocess.call(script_path, shell=True)
